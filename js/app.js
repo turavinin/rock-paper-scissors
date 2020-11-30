1d45d812b3cec15877f2b66527e1f89f49ebf289
turavinin <aturavinin@gmail.com>
2020-11-30 17:46:00 -0300

    Add new text

--- js/app.js
+++ js/app.js
@@ -1,37 +1,66 @@
 // Create variable to store the text element <p>
-const subtitleElement = document.querySelector('.subtitle-text');
+const subtitleElement = document.querySelectorAll('.subtitle-text');
 
 // Modify the innerHTML of <p>
-subtitleElement.innerHTML = subtitleElement.textContent
-  .split('')
-  .map((char) => {
-    return `<span class="faded-chars">${char}</span>`;
-  })
-  .join('');
+subtitleElement.forEach((sentence) => {
+  sentence.innerHTML = sentence.textContent
+    .split('')
+    .map((char) => {
+      return `<span class="faded-chars">${char}</span>`;
+    })
+    .join('');
+});
 
 // Create a variable to store the spans
 let charsToFade = document.querySelectorAll('.faded-chars');
 
-function fadeOut() {
+function fadeOut(sentence) {
   setTimeout(() => {
-    document.querySelectorAll('.faded-chars').forEach((char) => {
+    sentence.childNodes.forEach((char) => {
       char.classList.remove('fade-in');
     });
   }, 3000);
 }
 
-function startFade() {
+function startFade(sentence) {
   let counter = 0;
   let fadeInTimer = setInterval(() => {
-    let i = document.querySelectorAll('.faded-chars')[counter];
-    i.classList.add('fade-in');
+    sentence.childNodes[counter].classList.add('fade-in');
+    counter++;
+    if (counter == sentence.childNodes.length) {
+      clearInterval(fadeInTimer);
+      fadeOut(sentence);
+      let nextSentence = sentence.nextElementSibling;
+      startSentence(nextSentence);
+    }
+  }, 100);
+}
+
+/* function startFade(sentence) {
+  let counter = 0;
+  let fadeInTimer = setInterval(() => {
+    let currentSentence = document.querySelectorAll('.faded-chars')[counter];
+    currentSentence.classList.add('fade-in');
     console.log;
     counter++;
     if (counter == charsToFade.length) {
       clearInterval(fadeInTimer);
       fadeOut();
+      let nextSentence = subtitleElement.nextElementSibling;
+      startSentence(nextSentence);
     }
   }, 100);
 }
+ */
+function startSentence(sentence) {
+  if (!sentence) {
+    return;
+  } else {
+    startFade(sentence);
+  }
+}
 
-startFade();
+startSentence(document.querySelector('.subtitle-text'));
+
+console.log(document.querySelector('.subtitle-text').childNodes);
+console.log(document.querySelector('.faded-chars'));
